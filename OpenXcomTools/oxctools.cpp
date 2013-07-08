f4be78bddd82a904d13642cdbb80d17e439da3c3
Warboy <neo_nihilist@hotmail.com>
2013-07-09 04:23:39 +1000

    Start Building a tree.

--- OpenXcomTools/oxctools.cpp
+++ OpenXcomTools/oxctools.cpp
@@ -1,11 +1,58 @@
 #include "oxctools.h"
 #include "ui_oxctools.h"
+#include <QDialog>
+#include <QtCore>
+#include <QTreeWidgetItem>
 
 OXCTools::OXCTools(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::OXCTools)
 {
     ui->setupUi(this);
+    _countries = addParent("Countries");
+    _regions = addParent("Regions");
+    _facilities = addParent("Facilities");
+    _aircraft = addParent("Aircraft");
+    _aircraftXcom = addChild(_aircraft, "Xcom Crafts");
+    _aircraftUFO = addChild(_aircraft, "UFOs");
+    _items = addParent("Items");
+    _itemsAircraftWeapons = addChild(_items, "Aircraft Weapons");
+    _itemsHWPs = addChild(_items, "HWPs");
+    _itemsWeapons = addChild(_items, "Weapons");
+    _itemsCorpses = addChild(_items, "Corpses");
+    _itemsUFOComponents = addChild(_items, "UFO Components");
+    _inventories = addParent("Inventories");
+    _maps = addParent("Maps");
+    _mapsTerrain = addChild(_maps, "Terrains");
+    _mapsDeployment = addChild(_maps, "Deployments");
+    _armours = addParent("Armours");
+    _units = addParent("Units");
+    _unitsXcom = addChild(_units, "Xcom");
+    _unitsAlien = addChild(_units, "Alien");
+    _alienRaces = addParent("Alien Races");
+    _research = addParent("Research");
+    _manufacture = addParent("Manufacture");
+    _ufopaedia = addParent("Ufopaedia Articles");
+    _startBase = addParent("Starting Base");
+    _missions = addParent("Alien Missions");
+    _missionFlightPaths = addChild(_missions, "UFO Flight Paths");
+    _missionSpawns = addChild(_missions, "Ufo Spawn Procedures");
+    _missionLevels = addChild(_missions, "Equipment Levels");
+    _all = addParent("All");
+}
+QTreeWidgetItem *OXCTools::addParent(QString name)
+{
+    QTreeWidgetItem *itm = new QTreeWidgetItem(ui->treeWidget);
+    itm->setText(0, name);
+    return itm;
+}
+
+QTreeWidgetItem *OXCTools::addChild(QTreeWidgetItem *parent, QString name)
+{
+    QTreeWidgetItem *itm = new QTreeWidgetItem();
+    itm->setText(0, name);
+    parent->addChild(itm);
+    return itm;
 }
 
 OXCTools::~OXCTools()
--- OpenXcomTools/oxctools.h
+++ OpenXcomTools/oxctools.h
@@ -2,6 +2,9 @@
 #define OXCTOOLS_H
 
 #include <QMainWindow>
+#include <QDialog>
+#include <QtCore>
+#include <QTreeWidgetItem>
 
 namespace Ui {
 class OXCTools;
@@ -10,9 +13,18 @@ class OXCTools;
 class OXCTools : public QMainWindow
 {
     Q_OBJECT
+    QTreeWidgetItem *_countries, *_regions, *_facilities, *_aircraft, *_aircraftXcom, *_aircraftUFO, *_items;
+    QTreeWidgetItem *_itemsAircraftWeapons, *_itemsHWPs, *_itemsWeapons, *_itemsCorpses, *_itemsUFOComponents;
+    QTreeWidgetItem *_inventories, *_maps, *_mapsTerrain, *_mapsDeployment, *_armours, *_units, *_unitsXcom;
+    QTreeWidgetItem *_unitsAlien, *_alienRaces, *_research, *_manufacture, *_ufopaedia, *_startBase, *_missions;
+    QTreeWidgetItem *_missionFlightPaths, *_missionSpawns, *_missionLevels, *_all;
+    QTreeWidgetItem *addParent(QString name);
+    QTreeWidgetItem *addChild(QTreeWidgetItem *parent, QString name);
+    void SetupItemsListAircraft();
     
 public:
     explicit OXCTools(QWidget *parent = 0);
+
     ~OXCTools();
     
 private:
